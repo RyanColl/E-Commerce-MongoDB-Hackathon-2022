8c695e4672999c9dd007473a7eb66ea514908692
Irene Wu <72066810+irenewuu@users.noreply.github.com>
2022-01-03 14:07:40 -0800

    Merge pull request #3

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,18 +1,31 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 function NavBar() {
     return (
         <motion.div className='nav-bar'>
             <motion.div className='nav-left'>
-                <motion.div className='nav-item logo'></motion.div>
-                <motion.div className='nav-item home'></motion.div>
-                <motion.div className='nav-item browse'></motion.div>
+                <motion.div className='nav-item logo'>
+                    <motion.img src='./logo.png' animate={{scale: 0.6}} />
+                </motion.div>
+                <motion.div className='nav-item home'>
+                    <span>Home</span>
+                </motion.div>
+                <motion.div className='nav-item browse'>
+                    <span>Browse</span>
+                </motion.div>
             </motion.div>
             <motion.div className='nav-right'>
-                <motion.div className='nav-item search'></motion.div>
-                <motion.div className='nav-item account'></motion.div>
-                <motion.div className='nav-item cart'></motion.div>
+                <motion.div className='nav-item search'>
+                    <Icon icon="akar-icons:search" />
+                </motion.div>
+                <motion.div className='nav-item account'>
+                    <Icon icon="bi:person" />
+                </motion.div>
+                <motion.div className='nav-item cart'>
+                    <Icon icon="clarity:shopping-cart-line" />
+                </motion.div>
             </motion.div>
         </motion.div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function CustomApp({Component, pageProps}) {
       <>
         <Head>
           <title>Streetz</title>
-          <link rel="icon" href="/streetz.ico" />
+          <link rel="icon" href="./logo.ico" />
           <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
         </Head>
         <AppContext>
