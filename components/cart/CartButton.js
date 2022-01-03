36ff9046e9ab667713566b7006df708f08a75889
Ryan Collicutt <ryancollicutt22@gmail.com>
2022-01-03 11:44:40 -0800

    provider context explained

--- components/cart/CartButton.js
+++ components/cart/CartButton.js
@@ -4,12 +4,15 @@ import {useRouter} from 'next/router'
 import { AppProvider } from '../../context/AppContext'
 import { Icon } from '@iconify/react';
 function CartButton() {
+    //grabbing provider values (one value, one function)
     const {state, dispatch} = React.useContext(AppProvider)
+    // grabbing cart from state
     const { cart } = state
     return (
         <motion.div className='cart-button'>
             <Icon icon="bi:cart-check" color="white" width="32" height="32" />
             <span>{cart.length}</span>
+            <button onClick={() => dispatch({...state, cart: ['newcartitem']})}></button>
         </motion.div>
     )
 }
--- context/AppContext.js
+++ context/AppContext.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createContext, useState } from 'react'
+
+// provider created
 export const AppProvider = createContext()
+
+// provider component created
 function AppContext({children}) {
     const initialState = {
         cart: [], categories: [], products: []
