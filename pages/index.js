2dc53fe9a51db1f0fbbb888e776244bc70417525
rostrae <irenewu222@gmail.com>
2022-01-10 22:40:33 -0800

    homepage - carousel is there not functional yet

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { getProducts } from '../lib/dbAccess'
 import dbConnect from '../lib/dbConnect'
 import { AppProvider } from '../context/AppContext'
 import React from 'react'
+import leftarrow from '../assets/left_arrow.svg'
+import rightarrow from '../assets/right_arrow.svg'
+import ProductPreview from '../components/productPreview/productPreview'
 
 function index({products}) {
   const {state, dispatch} = React.useContext(AppProvider)
@@ -17,9 +20,43 @@ function index({products}) {
       console.log('products now added to state')
     }
   }, [state.products])
+
   return (
-    <div>
-      
+    <div className='centered-cont'>
+      <img src="./placeholder.jpg" className='hero-img' />
+
+      <div className='carousel-title flex-row-space-between'>
+        <h4>Trending now</h4>
+        <div className='carousel-arrows'>
+          <img src={leftarrow.src} />
+          <img src={rightarrow.src} />
+        </div>
+      </div>
+
+      <div className='carousel-cont'>
+        <div className='carousel'>
+          {products.map((product, i) => {
+            return (
+                <ProductPreview 
+                    i={i}
+                    src={product.image[0] || product.image[1]} 
+                    prodTitle={product.title} 
+                    prodPrice={`${product.price}`}
+                    prodPage={() => router.push(`products/${product._id}`)}
+                />
+            );
+          })}
+        </div>
+
+      </div>
+
+      <div className='collection-cont'>
+        <img src="./placeholder.jpg" />
+        <div>
+          <h3 className='uppercased'>View the Luxury Collection</h3>
+          <button type="button" className='white-btn'>Discover Now</button>
+        </div>
+      </div>
     </div>
   )
 }
